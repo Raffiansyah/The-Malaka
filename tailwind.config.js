8c74a1ed862c77a5b8d9b67b1b978918ab578c89
Mohammad Raffiansyah <mohammad.raffiansyah007@gmail.com>
2022-04-17 19:36:14 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        sky: '#00BAC7',
+        secondary: '#EDFCFD'
+      },
+      backgroundImage: {
+        hero: "url('/public/asset/Group 27.svg')",
+        book: "url('/public/asset/Rectangle 20.png')",
+        foot: "url('/public/asset/Rectangle 643.png')"
+      }
+    },
+  },
+  plugins: [],
+}
